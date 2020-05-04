ec40dccd8cbbf8443b97b6fcf3772bcd3a787f8b
Frykers <tonyh.wgtn@gmail.com>
2020-05-05 11:54:03 +1200

    Created and named functions

---
+++ heart-rate.py
@@ -0,0 +1,25 @@
+#heart-rate.py
+#prototype heart-rate tracker for fitness watch
+#By: Tony Huang
+#Created 5/5/2020
+#Last modified 5/5/2020
+#version 0.1
+
+def int_input():
+    """Gets integer input and checks it for validity before returning it"""
+
+def heart_rates():
+    """Gets input on users heart rates and calculate resting rate"""
+
+def intensity():
+    """Determins intensity of exercise based of heart_rate input"""
+
+def exercise_selector():
+    """Allows user to select the exercise they are performing"""
+
+def output():
+    """Disaplays completed exercise, length of time, and intensity"""
+
+def main():
+    """Main sequence"""
+
